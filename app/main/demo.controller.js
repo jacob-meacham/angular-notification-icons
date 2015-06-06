4b7eb7d4bac1c6041ac4fed2617f397be639195f
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-06-05 23:44:47 -0700

    Add autocounter.

--- app/main/demo.controller.js
+++ app/main/demo.controller.js
@@ -1,10 +1,25 @@
 'use strict';
 
-var DemoController = function() {
+var DemoController = function($interval) {
   var vm = this;
 
-  vm.pending = [0];
+  var autoCounter = function(counterIdx, start, steps, delay) {
+    var numIterations = 0;
+    vm.pending[counterIdx] = start;
+    $interval(function() {
+      if (++numIterations > steps) {
+        vm.pending[counterIdx] = start;
+        numIterations = 0;
+      } else {
+        vm.pending[counterIdx] = vm.pending[counterIdx] + 1;
+      }
+    }, delay);
+  };
+  
+
+  vm.pending = [0, 0];
+  autoCounter(1, 1, 9, 700);
 };
 
 angular.module('angular-notifications.demo.controllers')
-  .controller('DemoController', [DemoController]);
+  .controller('DemoController', ['$interval', DemoController]);
