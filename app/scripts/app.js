c127b42f2fc27c14a1d68ff92712578263fff5b9
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-06-07 18:17:14 -0700

    Make app a little simpler.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,23 @@
 'use strict';
 
+var app = angular.module('angular-notifications.demo', ['ngRoute', 'angular-notifications', 'hljs', 'angular-notifications.demo.controllers']);
+
+// Pre-define modules
+angular.module('angular-notifications.demo.controllers', []);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  $routeProvider.
+    when('/', {
+      controller: 'DemoController',
+      controllerAs: 'vm'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+
+    $locationProvider.html5Mode(true);
+}]);
+
 var DemoController = function($interval) {
   var vm = this;
 
