431cee5d48a928c73f7b6bb354acbdaaad21a22c
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-06-07 18:30:06 -0700

    Update github pages.

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('angular-notifications.demo', ['ngRoute', 'angular-notifications', 'angular-notifications.demo.controllers']);
+var app = angular.module('angular-notifications.demo', ['ngRoute', 'angular-notifications', 'hljs', 'angular-notifications.demo.controllers']);
 
 // Pre-define modules
 angular.module('angular-notifications.demo.controllers', []);
@@ -17,3 +17,33 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
 
     $locationProvider.html5Mode(true);
 }]);
+
+var DemoController = function($interval) {
+  var vm = this;
+
+  var autoCounter = function(index, start, steps, delay) {
+    var numIterations = 0;
+    vm.autoPending[index] = start;
+    $interval(function() {
+      if (++numIterations > steps) {
+        vm.autoPending[index] = start;
+        numIterations = 0;
+      } else {
+        vm.autoPending[index] = vm.autoPending[index] + 1;
+      }
+    }, delay);
+  };
+  
+
+  vm.autoPending = [0, 0];
+  autoCounter(0, 1, 4, 1000);
+  autoCounter(1, 98, 4, 1000);
+
+  vm.scriptSrc = '<link rel="stylesheet" href="bower_components/angular-notifications/angular-notifications.min.css">\n\n' +
+'<script src="bower_components/angular/angular.js"></script>\n' +
+'<script src="bower_components/angular-animate/angular-animate.js"></script>\n' +
+'<script src="bower_components/angular-notifications/angular-notifications.min.js"></script>';
+};
+
+angular.module('angular-notifications.demo.controllers')
+  .controller('DemoController', ['$interval', DemoController]);
