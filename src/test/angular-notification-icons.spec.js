1e9e2c08076e1729a4a57960769db3ae741944e3
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-06-10 23:39:48 -0800

    Stub out a bit more of the testing.

--- src/test/angular-notification-icons.spec.js
+++ src/test/angular-notification-icons.spec.js
@@ -1,9 +1,22 @@
 'use strict';
 
-describe('NotificationService', function() {
-  beforeEach(module('angular-notifications'));
+describe('angular-notifications-icon', function() {
+  beforeEach(module('angular-notification-icons'));
 
-  it('should pass', function(done) {
-    done();
+  describe('NotificationDirectiveController', function() {
+    var $controller;
+    var $rootScope;
+    var $animate;
+
+    beforeEach(inject(function(_$controller_, _$rootScope_, _$animate_) {
+      $controller = _$controller_;
+      $rootScope = _$rootScope_;
+      $animate = _$animate_;
+    }));
+
+    it('should pass', function(done) {
+      $controller('NotificationDirectiveController', {$scope: $rootScope.$new()});
+      done();
+    });
   });
 });
