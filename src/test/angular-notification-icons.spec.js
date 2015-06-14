55bbb4d42fcb976c97cfbbabc27205ee3c1e7caf
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-06-13 23:01:48 -0800

    Add a few tests and stub out a lot more.

--- src/test/angular-notification-icons.spec.js
+++ src/test/angular-notification-icons.spec.js
@@ -5,18 +5,118 @@ describe('angular-notifications-icon', function() {
 
   describe('NotificationDirectiveController', function() {
     var $controller;
-    var $rootScope;
+    var scope;
     var $animate;
+    var $q;
 
-    beforeEach(inject(function(_$controller_, _$rootScope_, _$animate_) {
+    var sandbox;
+    var element;
+    var angularElement;
+
+    beforeEach(function() {
+      sandbox = sinon.sandbox.create();
+
+      element = { name: 'foo' };
+      angularElement = {
+        find: function() {
+          return element;
+        }
+      };
+    });
+
+    afterEach(function() {
+      sandbox.restore();
+    });
+
+    beforeEach(inject(function(_$controller_, $rootScope, _$animate_, _$q_) {
       $controller = _$controller_;
-      $rootScope = _$rootScope_;
+      scope = {$scope: $rootScope.$new()};
       $animate = _$animate_;
+      $q = _$q_;
     }));
 
-    it('should pass', function(done) {
-      $controller('NotificationDirectiveController', {$scope: $rootScope.$new()});
-      done();
+    var spyOn = function(object, functionName) {
+      if (!object[functionName]) {
+        var spy = sandbox.spy();
+        object[functionName] = spy;
+        return spy;
+      }
+
+      return sandbox.spy(object, functionName);
+    };
+
+    it('should start with reasonable defaults', function() {
+      var ctrl = $controller('NotificationDirectiveController', scope);
+      ctrl.visible.should.be.false;
+      ctrl.wideThreshold.should.eql(100);
+    });
+
+    it('should initialize with an element', function() {
+      var findSpy = spyOn(angularElement, 'find');
+
+      var ctrl = $controller('NotificationDirectiveController', scope);
+      ctrl.init(angularElement);
+      findSpy.getCall(0).args[0].should.equal('.angular-notifications-icon');
+    });
+
+    it('should set a clear trigger if specified', function() {
+      var onSpy = spyOn(angularElement, 'on');
+
+      var ctrl = $controller('NotificationDirectiveController', scope, {clearTrigger: 'mouseover'});
+      ctrl.init(angularElement);
+      onSpy.getCall(0).args[0].should.eql('mouseover');
+    });
+
+    it('should appear if not visible and count goes above 0', function() {
+
+    });
+
+    it('should play appear animation if set', function() {
+
+    });
+
+    it('should use a default appear animation', function() {
+
+    });
+
+    it('should update if visible and the count changes', function() {
+
+    });
+
+    it('should play update animation if set', function() {
+
+    });
+
+    it('should use a default update animation', function() {
+
+    });
+
+    it('should set the appear animation based on the \'animation\' property', function() {
+
+    });
+
+    it('should set the update animation based on the \'animation\' property', function() {
+
+    });
+
+    it('should disappear if visible and the count goes to or below 0', function() {
+
+    });
+
+    it('should play disappear animation if set', function() {
+
+    });
+
+    it('should interrupt an aniamtion that is already playing', function() {
+
+    });
+
+    it('should add .wide-icon if the count is higher than the wide threshold', function() {
+
+    });
+
+    it('should remove .wide-icon if the count is lower than the wide threshold', function() {
+
     });
   });
 });
